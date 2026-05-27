cab78c8f8e7124d4cb48e70b4ed889f21f789e0e
Meowouuo <9142634@qq.com>
2026-05-27 23:51:08 +0800

    fix: 歌词文件改用__songPush接口(黑暗中漫舞.js)

--- lyrics/黑暗中漫舞.js
+++ lyrics/黑暗中漫舞.js
@@ -40,7 +40,9 @@
             { chars: ["难","道","你","很","想","天","使","问","我"," ","我","的","舞","步","跳","得","可","美"], jp: ["naan4","dou6","nei5","han2","soeng2","tin1","si2","man6","ngo5","","ngo5","dik1","mou5","bou6","tiu3","dak1","ho2","mei5"] }
         ]
     };
-    if (typeof window !== 'undefined' && window.__songs) {
-        window.__songs.push(song);
+    // 【方案A】通过 __songPush 接口注册歌曲数据
+    // loadSongLyrics() 会将 __songPush 替换为当前歌曲的唯一数组
+    if (typeof window !== 'undefined' && window.__songPush) {
+        window.__songPush(song);
     }
 })();
