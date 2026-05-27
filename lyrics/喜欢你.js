bc8b012c61af18dac1542562ca44d0c1cdc3aa96
Meowouuo <9142634@qq.com>
2026-05-27 23:48:09 +0800

    fix: 歌词文件改用__songPush接口(喜欢你.js)

--- lyrics/喜欢你.js
+++ lyrics/喜欢你.js
@@ -45,7 +45,9 @@
             { chars: ["挽","手","说","梦","话"," ","像","昨","天"," ","你","共","我"], jp: ["waan5","sau2","syut3","mung6","waa6","","zoeng6","zok3","tin1","","nei5","gung6","ngo5"] }
         ]
     };
-    if (typeof window !== 'undefined' && window.__songs) {
-        window.__songs.push(song);
+    // 【方案A】通过 __songPush 接口注册歌曲数据
+    // loadSongLyrics() 会将 __songPush 替换为当前歌曲的唯一数组
+    if (typeof window !== 'undefined' && window.__songPush) {
+        window.__songPush(song);
     }
 })();
